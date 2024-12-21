a5599480048cafb81a16fd7dec629717b89a4f64
DariaK05 <71390242+DariaK05@users.noreply.github.com>
2024-12-22 00:05:54 +0300

    feat: implement aiogram calendar

--- source/Bot/handlers/statistics.py
+++ source/Bot/handlers/statistics.py
@@ -4,14 +4,15 @@ import time
 import hashlib
 from aiogram import Router, types, F
 from aiogram.fsm.context import FSMContext
+from aiogram.filters.callback_data import CallbackData
 from aiogram.filters.command import Command
 from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import CallbackQuery
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram_calendar import SimpleCalendar, SimpleCalendarCallback, get_user_locale
 from aiofile import async_open
-from calendar import monthrange
 from pathlib import Path
 from source.Bot.messages import stats_msg, get_rqst_msg
-from source.Bot.dates import months, months_id
 from source.Analyzer.AnalyzerDataTypes import from_dict
 from source.Analyzer.AnalyzerDataTypes import SharesPortfolioIntervalConnectorRequest, SharesPortfolioIntervalAnalyzerResponse
 from source.Bot.request_former import form_request
@@ -25,11 +26,7 @@ class Request(StatesGroup):
     encoded_token = State()
     security_type = State()
     request_type = State()
-    start_year = State()
-    start_month = State()
     start_date = State()
-    end_year = State()
-    end_month = State()
     end_date = State()
 
 @router.message(F.text, Command("stats"))
@@ -76,156 +73,89 @@ async def get_total_stats(message: types.Message, state: FSMContext):
     await message.answer("К сожалению, Ваш запрос пока не поддерживается")
     # TODO: add request processing
 
-
 @router.message(Request.request_type, F.text.lower() == "с открытия счёта до определённой даты")
 async def get_up_to_date_stats(message: types.Message, state: FSMContext):
-    await state.update_data(start_year=None)
-    await state.set_state(Request.end_year)
-    builder = ReplyKeyboardBuilder()
-    builder.add(types.KeyboardButton(text=str(datetime.date.today().year)))
+    await state.update_data(start_date=None)
+    await state.set_state(Request.end_date)
+    calendar = SimpleCalendar(
+        locale=await get_user_locale(message.from_user), show_alerts=True
+    )
+    calendar.set_dates_range(datetime.datetime(2024, 1, 1), datetime.datetime.today()) # replace with portfolio creation
     await message.answer('''Для получения статистики за период с открытия счёта \
-до определённой даты, введите год \(или выберите текущий\) и выберите месяц и число окончания''',
-                         reply_markup=builder.as_markup(resize_keyboard=True,
-                                                        input_field_placeholder="Год окончания",
-                                                        one_time_keyboard=True))
-
+до определённой даты, выберите дату окончания периода''',
+                         reply_markup=await calendar.start_calendar(year=2024, month=1))
 
 @router.message(Request.request_type, F.text.lower() == "за определённый период")
 async def get_period_stats(message: types.Message, state: FSMContext):
-    await state.set_state(Request.start_year)
-    builder = ReplyKeyboardBuilder()
-    builder.add(types.KeyboardButton(text=str(datetime.date.today().year)))
+    await state.set_state(Request.start_date)
+    calendar = SimpleCalendar(
+        locale=await get_user_locale(message.from_user), show_alerts=True
+    )
+    calendar.set_dates_range(datetime.datetime(2024, 1, 1),
+                             datetime.datetime.today())  # replace with portfolio creation
     await message.reply('''Для получения статистики за определённый период \
 сначала введите год \(или выберите текущий\) и выберите месяц и число его начала, \
 затем введите год и выберите месяц и число его окончания''',
-                         reply_markup=builder.as_markup(resize_keyboard=True,
-                                                        input_field_placeholder="Год начала",
-                                                        one_time_keyboard=True))
+                         reply_markup=await calendar.start_calendar(year=2024, month=1))
 
-@router.message(Request.start_year, lambda message: message.text.isdigit())
-async def get_start_year(message: types.Message, state: FSMContext):
-    await state.update_data(start_year=int(message.text))
-    builder = ReplyKeyboardBuilder()
-    for month in months:
-        builder.add(types.KeyboardButton(text=month))
-    builder.adjust(3, 3, 3, 3)
-    await state.set_state(Request.start_month)
-    await message.reply("Отлично\! Теперь выберите месяц начала",
-                         reply_markup=builder.as_markup(resize_keyboard=True,
-                                                        input_field_placeholder="Месяц начала",
-                                                        one_time_keyboard=True))
+@router.callback_query(Request.start_date, SimpleCalendarCallback.filter())
+async def get_start_date(callback_query: CallbackQuery, callback_data: CallbackData, state: FSMContext):
+    calendar = SimpleCalendar(
+        locale=await get_user_locale(callback_query.from_user), show_alerts=True
+    )
+    calendar.set_dates_range(datetime.datetime(2024, 1, 1), datetime.datetime.today())
+    selected, date = await calendar.process_selection(callback_query, callback_data)
+    if selected:
+        await state.update_data(start_date=date)
+        await state.set_state(Request.end_date)
+        calendar = SimpleCalendar(
+            locale=await get_user_locale(callback_query.from_user), show_alerts=True
+        )
+        calendar.set_dates_range(datetime.datetime(2024, 1, 1),
+                                 datetime.datetime.today())  # replace with portfolio creation
+        await callback_query.message.answer("Отлично\! Теперь выберите дату окончания",
+                                            reply_markup=await calendar.start_calendar(year=2024, month=1))
 
-@router.message(Request.start_month, lambda message: message.text.upper() in months)
-async def get_start_month(message: types.Message, state: FSMContext):
-    await state.update_data(start_month=months_id[message.text.upper()])
-    await state.set_state(Request.start_date)
-    builder = ReplyKeyboardBuilder()
-    data = await state.get_data()
-    month_len = monthrange(data["start_year"], data["start_month"])[1]
-    for i in range(1, month_len + 1):
-        builder.add(types.KeyboardButton(text=str(i)))
-    builder.adjust(5, 5, 5, 5, 5, month_len - 25)
-    await message.reply("Отлично\! Теперь выберите число начала",
-                         reply_markup=builder.as_markup(resize_keyboard=True,
-                                                        input_field_placeholder="Число начала",
-                                                        one_time_keyboard=True))
-
-@router.message(Request.start_date, lambda message: message.text.isdigit())
-async def get_start_date(message: types.Message, state: FSMContext):
-    data = await state.get_data()
-    month_len = monthrange(data["start_year"], data["start_month"])[1]
-    if 1 <= int(message.text) <= month_len:
-        await state.update_data(start_date=int(message.text))
-        await state.set_state(Request.end_year)
-        builder = ReplyKeyboardBuilder()
-        builder.add(types.KeyboardButton(text=str(datetime.date.today().year)))
-        await message.reply("Отлично\! Теперь введите год окончания",
-                            reply_markup=builder.as_markup(resize_keyboard=True,
-                                                           input_field_placeholder="Год окончания",
-                                                           one_time_keyboard=True))
-    else:
-        builder = ReplyKeyboardBuilder()
-        for i in range(1, month_len + 1):
-            builder.add(types.KeyboardButton(text=str(i)))
-        builder.adjust(5, 5, 5, 5, 5, month_len - 25)
-        await message.reply("Выберите корректную дату начала",
-                            reply_markup=builder.as_markup(resize_keyboard=True,
-                                                           input_field_placeholder="Число начала",
-                                                           one_time_keyboard=True))
-
-@router.message(Request.end_year, lambda message: message.text.isdigit())
-async def get_end_year(message: types.Message, state: FSMContext):
-    await state.update_data(end_year=int(message.text))
-    await state.set_state(Request.end_month)
-    builder = ReplyKeyboardBuilder()
-    for month in months:
-        builder.add(types.KeyboardButton(text=month))
-    builder.adjust(3, 3, 3, 3)
-    await message.reply("Отлично\! Теперь выберите месяц окончания",
-                        reply_markup=builder.as_markup(resize_keyboard=True,
-                                                       input_field_placeholder="Месяц окончания",
-                                                       one_time_keyboard=True))
-
-@router.message(Request.end_month, lambda message: message.text.upper() in months)
-async def get_end_month(message: types.Message, state: FSMContext):
-    await state.update_data(end_month=months_id[message.text.upper()])
-    await state.set_state(Request.end_date)
-    builder = ReplyKeyboardBuilder()
-    data = await state.get_data()
-    month_len = monthrange(data["end_year"], data["end_month"])[1]
-    for i in range(1, month_len + 1):
-        builder.add(types.KeyboardButton(text=str(i)))
-    builder.adjust(5, 5, 5, 5, 5, month_len - 25)
-    await message.reply("Отлично\! Теперь выберите число окончания",
-                         reply_markup=builder.as_markup(resize_keyboard=True,
-                                                        input_field_placeholder="Число окончания",
-                                                        one_time_keyboard=True))
-
-@router.message(Request.end_date, lambda message: message.text.isdigit())
-async def get_end_date(message: types.Message, state: FSMContext):
-    data = await state.get_data()
-    month_len = monthrange(data["end_year"], data["end_month"])[1]
-    if 1 <= int(message.text) <= month_len:
-        await state.update_data(end_date=int(message.text))
+@router.callback_query(Request.end_date, SimpleCalendarCallback.filter())
+async def get_end_date(callback_query: CallbackQuery, callback_data: CallbackData, state: FSMContext):
+    calendar = SimpleCalendar(
+        locale=await get_user_locale(callback_query.from_user), show_alerts=True
+    )
+    calendar.set_dates_range(datetime.datetime(2024, 1, 1), datetime.datetime.today())
+    selected, date = await calendar.process_selection(callback_query, callback_data)
+    if selected:
+        await state.update_data(end_date=date)
         data = await state.get_data()
-        if data["start_year"] is not None:
-            request = await form_request(data["security_type"],
-                                         datetime.date(data["start_year"], data["start_month"], data["start_date"]),
-                                         datetime.date(data["end_year"], data["end_month"], data["end_date"]))
-            request.token = token_encoder.decode_token(data["encoded_token"])
-            request_time_str = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
-            request_file_name = f"request_shares_{hashlib.sha256((request.token + request_time_str).encode('utf-8')).hexdigest()}.json"
-            request_path = "../../connector_requests/" + request_file_name
-            async with async_open(request_path, 'wb') as f:
-                await f.write(orjson.dumps(request))
-            response_file_name = f"response_shares_{hashlib.sha256((request.token + request_time_str).encode('utf-8')).hexdigest()}.json"
-            response_path = "../../analyzer_responses/" + response_file_name
-            results = None
-            total_sleep_time = 0
-            while not results and total_sleep_time < 10:
-                time.sleep(0.5)
-                total_sleep_time += 0.5
-                if Path(response_path).exists():
-                    async with async_open(response_path, 'rb') as f:
-                        contents = await f.read()
-                        results = from_dict(SharesPortfolioIntervalAnalyzerResponse, orjson.loads(contents))
+
+        request = await form_request(data["security_type"], data["start_date"].date(), data["end_date"].date())
+        request.token = token_encoder.decode_token(data["encoded_token"])
+
+        request_time_str = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
+        request_file_name = f"request_shares_{hashlib.sha256((request.token + request_time_str).encode('utf-8')).hexdigest()}.json"
+        request_path = "../../connector_requests/" + request_file_name
+
+        async with async_open(request_path, 'wb') as f:
+            await f.write(orjson.dumps(request))
+
+        response_file_name = f"response_shares_{hashlib.sha256((request.token + request_time_str).encode('utf-8')).hexdigest()}.json"
+        response_path = "../../analyzer_responses/" + response_file_name
+
+        results = None
+        total_sleep_time = 0
+        while not results and total_sleep_time < 10:
+            time.sleep(0.5)
+            total_sleep_time += 0.5
             if Path(response_path).exists():
-                Path(response_path).unlink()
-            if not results:
-                await message.answer("К сожалению, Ваш запрос не удалось обработать\. Попробуйте позже или скорректируйте запрос")
-            else:
-                result_msg = await form_result(results)
-                await message.answer(result_msg)
+                async with async_open(response_path, 'rb') as f:
+                    contents = await f.read()
+                    results = from_dict(SharesPortfolioIntervalAnalyzerResponse, orjson.loads(contents))
+
+        if Path(response_path).exists():
+            Path(response_path).unlink()
+        if not results:
+            await callback_query.message.answer(
+                "К сожалению, Ваш запрос не удалось обработать\. Попробуйте позже или скорректируйте запрос")
         else:
-            await message.answer("К сожалению, Ваш запрос пока не поддерживается")
-            # TODO: add request processing
+            result_msg = await form_result(results)
+            await callback_query.message.answer(result_msg)
         await state.clear()
-    else:
-        builder = ReplyKeyboardBuilder()
-        for i in range(1, month_len + 1):
-            builder.add(types.KeyboardButton(text=str(i)))
-        builder.adjust(5, 5, 5, 5, 5, month_len - 25)
-        await message.reply("Выберите корректную дату окончания",
-                            reply_markup=builder.as_markup(resize_keyboard=True,
-                                                           input_field_placeholder="Число окончания",
-                                                           one_time_keyboard=True))
